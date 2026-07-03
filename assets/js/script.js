9d3b99c0bc264cf692a7b30da2cb1e0d505331fe
Noha1203 <zanobonidario@gmail.com>
2026-07-04 00:36:37 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+function vote(faction){
 
+// blocca doppio voto
+if(localStorage.getItem("vote")) return;
+
+localStorage.setItem("vote", faction);
+
+// effetto click
+document.body.style.transform = "scale(0.98)";
+
+setTimeout(() => {
+window.location.href = "grazie.html";
+}, 600);
+
+}
