ab73f45f68b7aaaba90a2b0e553cc7f43ac6772b
Noha1203 <zanobonidario@gmail.com>
2026-07-04 12:54:54 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,9 +22,6 @@ async function vote(faction) {
 
         const response = await fetch(API_URL, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
             body: JSON.stringify(payload)
         });
 
